6d846f9d812301f6270d4b78d08ed65976b33962
daveycodez <david@swiftapk.com>
2025-03-10 19:39:28 -0700

    Manually check cookies in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { type NextRequest, NextResponse } from "next/server"
 
 export async function middleware(request: NextRequest) {
-    // const sessionCookie = getSessionCookie(request) // Broken?
+    // const sessionCookie = getSessionCookie(request) // Broken
 
     if (!request.cookies.has("better-auth.session_token")) {
         const redirectTo = request.nextUrl.pathname + request.nextUrl.search
-        // return NextResponse.redirect(new URL(`/auth/sign-in?redirectTo=${redirectTo}`, request.url))
+        return NextResponse.redirect(new URL(`/auth/sign-in?redirectTo=${redirectTo}`, request.url))
     }
 
     return NextResponse.next()
