ea97797a5c95f582be471f25b83e105a5b71d99b
daveycodez <david@swiftapk.com>
2025-03-22 21:23:26 +0100

    Get off better-auth beta

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,12 @@
-import { getSessionCookie } from "better-auth/cookies"
 import { type NextRequest, NextResponse } from "next/server"
 
 export async function middleware(request: NextRequest) {
-    const sessionCookie = getSessionCookie(request)
+    // const sessionCookie = getSessionCookie(request)
 
-    if (!sessionCookie) {
-        const redirectTo = request.nextUrl.pathname + request.nextUrl.search
-        return NextResponse.redirect(new URL(`/auth/sign-in?redirectTo=${redirectTo}`, request.url))
-    }
+    // if (!sessionCookie) {
+    //     const redirectTo = request.nextUrl.pathname + request.nextUrl.search
+    //     return NextResponse.redirect(new URL(`/auth/sign-in?redirectTo=${redirectTo}`, request.url))
+    // }
 
     return NextResponse.next()
 }
