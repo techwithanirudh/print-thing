fd6b216657a402f1fc719fcea4da29ee78abf10d
daveycodez <daveycodez@gmail.com>
2025-04-22 04:15:00 -0700

    Latest packages, better middleware cache clearing

--- app/auth/[pathname]/view.tsx
+++ app/auth/[pathname]/view.tsx
@@ -2,19 +2,12 @@
 
 import { AuthCard } from "@daveyplate/better-auth-ui"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 
 import { cn } from "@/lib/utils"
 
 export function AuthView({ pathname }: { pathname: string }) {
     // Just an example, SettingsCards already includes this
     // useAuthenticate({ enabled: pathname === "settings" })
-    const router = useRouter()
-
-    useEffect(() => {
-        router.refresh()
-    }, [router])
 
     return (
         <main className="flex grow flex-col items-center justify-center gap-4 p-4">
