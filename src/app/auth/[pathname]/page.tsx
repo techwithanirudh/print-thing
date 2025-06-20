4dc8c7235cf2c17e3adea8e15f89748a7ab5db47
daveycodez <daveycodez@gmail.com>
2025-06-20 01:24:12 -0700

    Update page.tsx

--- src/app/auth/[pathname]/page.tsx
+++ src/app/auth/[pathname]/page.tsx
@@ -29,7 +29,16 @@ export default async function AuthPage({
 
     return (
         <main className="container flex grow flex-col items-center justify-center gap-4 self-center p-4 md:p-6">
-            <AuthCard pathname={pathname} />
+            <AuthCard
+                classNames={{
+                    settings: {
+                        sidebar: {
+                            base: "sticky top-20"
+                        }
+                    }
+                }}
+                pathname={pathname}
+            />
 
             {["sign-in", "sign-up", "forgot-password"].includes(pathname) && (
                 <p className="text-muted-foreground text-xs">
